5cf985278bda0fa7b67a36148b72ce9fe39f4336
Yreyaaa <aamrazr@gmail.com>
2024-01-04 13:32:58 +0300

    Rename file

--- TaskMangSystem/src/main/java/ru/yreyaaa/taskmanagementsystem/TaskManagementSystem/models/Priority.java
+++
@@ -1,3 +0,0 @@
-package ru.yreyaaa.taskmanagementsystem.TaskManagementSystem.models;
-
-public enum Priority {LOW, MEDIUM, HIGH}
--- TaskMangSystem/src/main/java/ru/yreyaaa/taskmanagementsystem/TaskManagementSystem/models/Status.java
+++
@@ -1,3 +0,0 @@
-package ru.yreyaaa.taskmanagementsystem.TaskManagementSystem.models;
-
-public enum Status {IN_PROGRESS, DONE, PAUSED}
